c9c6a8ce80ae4885b4a68aec4c5e834cd7fce396
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-12-09 01:03:03 +0300

    Update and delete triggers

--- mysql_trigger_lab.sql
+++ mysql_trigger_lab.sql
@@ -1,5 +1,7 @@
 DELIMITER $$
 
+-- Insert ---
+
 CREATE TRIGGER ProductSellPriceInsertCheck AFTER INSERT ON Products FOR EACH ROW
 BEGIN
   IF NEW.SellPrice < NEW.BuyPrice THEN
@@ -8,6 +10,23 @@ BEGIN
   END IF;
 END$$
 
+-- Update --
+
+CREATE TRIGGER ProductSellPriceUpdateCheck AFTER UPDATE ON Products FOR EACH ROW
+BEGIN
+  IF NEW.SellPrice < NEW.BuyPrice THEN
+    INSERT INTO Notifications VALUES 
+    (NULL, CONCAT(NEW.ProductID, " was updated with a SellPrice of ", NEW.SellPrice, " which is the same or less than the BuyPrice"), NOW());
+  END IF;
+END$$
+
+-- Delete --
+
+CREATE TRIGGER NotifyProductDelete AFTER DELETE ON Products FOR EACH ROW
+BEGIN
+  INSERT INTO Notifications VALUES 
+  (NULL, CONCAT("The product with a ProductID ", OLD.ProductID, " was deleted"), NOW());
+END$$
+
+-- End of triggers.
 DELIMITER ;
-
-
