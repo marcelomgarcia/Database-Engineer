49056d5f3fae93a536b2fb4432941c3eabd24fbb
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-12-21 18:35:44 +0300

    Invalid chacter from copy

--- luckyshrub_db_5.sql
+++ luckyshrub_db_5.sql
@@ -4,7 +4,7 @@ USE Lucky_Shrub;
 
 CREATE TABLE IF NOT EXISTS Orders (OrderID INT NOT NULL PRIMARY KEY, ClientID VARCHAR(10), ProductID VARCHAR(10), Quantity INT, Cost DECIMAL(6,2), Date DATE);
 CREATE TABLE IF NOT EXISTS Products (ProductID VARCHAR(10), ProductName VARCHAR(100), BuyPrice DECIMAL(6,2), SellPrice DECIMAL(6,2), NumberOfItems INT);
-CREATE TABLE IF NOT EXISTS Activity (ActivityID INT PRIMARY KEY, Properties JSON ); 
+CREATE TABLE IF NOT EXISTS Activity (ActivityID INT PRIMARY KEY, Properties JSON );
 
 INSERT INTO Orders (OrderID, ClientID, ProductID , Quantity, Cost, Date) VALUES 
 (1, "Cl1", "P1", 10, 500, "2020-09-01" ), 
