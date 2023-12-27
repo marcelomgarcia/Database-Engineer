a70dd37c48310faaf092a49c321c0c30da7a1c1b
Marcelo Garcia <garcm0b@KAUST.EDU.SA>
2023-12-27 11:56:44 +0300

    Initial commit

---
+++ data_analysis_cte.sql
@@ -0,0 +1,9 @@
+with
+SalesP4in2020 as (select concat(sum(Quantity), " (2020)" as "P4 product: Quantity sold" from Orders where year(Date) = 2020 and ProductID = "P4"),
+SalesP4in2021 as (select concat(sum(Quantity), " (2021)" from Orders where year(Date) = 2021 and ProductID = "P4"),
+SalesP4in2022 as (select concat(sum(Quantity), " (2022)" from Orders where year(Date) = 2022 and ProductID = "P4")
+select * from SalesP4in2020
+union
+select * from SalesP4in2021
+union
+select * from SalesP4in2022;
